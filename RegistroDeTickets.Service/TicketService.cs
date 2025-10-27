fd778b3e38c4427380e979526190435553932ea2
Diego Molver <molverdiego@gmail.com>
2025-10-27 03:20:43 -0300

    Creo la clase TicketRepository y separo en capas

--- RegistroDeTickets.Service/TicketService.cs
+++ RegistroDeTickets.Service/TicketService.cs
@@ -13,39 +13,30 @@ namespace RegistroDeTickets.Service
 
     public class TicketService : ITicketService
     {
-        ITicketRepository _ticketRepository;
+        private readonly ITicketRepository _ticketRepository;
         public void AgregarTicket(Ticket ticket)
         {
-            RegistroDeTicketsPw3Context ctx = new RegistroDeTicketsPw3Context();
-
             //ticket.Id = _tickets.Count + 1;  --> Entiendo que con el IDENTITY ya no es necesario calcular el ID manualmente
             ticket.Estado = "Abierto"; // Este estado se podria poner en un DEFAULT de la base de datos?
             ticket.FechaCreacion = DateTime.Now; // Este campo tambien podria ser un DEFAULT en la base de datos?
-
             ticket.IdUsuario = 1; // Por ahora asigno un usuario fijo, luego se debe obtener el usuario logueado
 
-            ctx.Tickets.Add(ticket);
-            ctx.SaveChanges();
+            _ticketRepository.AgregarTicket(ticket);
         }
 
         public List<Ticket> ObtenerTickets()
         {
-            RegistroDeTicketsPw3Context ctx = new RegistroDeTicketsPw3Context();
-
-            return ctx.Tickets.ToList();
+            return _ticketRepository.ObtenerTickets();
         }
 
         public void EditarTicket(Ticket ticket)
         {
-
+            _ticketRepository.EditarTicket(ticket);
         }
 
         public void EliminarTicket(Ticket ticket)
         {
-            RegistroDeTicketsPw3Context ctx = new RegistroDeTicketsPw3Context();
-
-            ctx.Tickets.Remove(ctx.Tickets.Find(ticket.Id));
-            ctx.SaveChanges();
+            _ticketRepository.EliminarTicket(ticket);
         }
 
     }
